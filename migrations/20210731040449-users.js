87d4dc32fa081bd3cd32e978d9c0366b44777b36
Lorena Góes <goesloren@gmail.com>
2021-08-02 01:55:23 -0300

    feat user migration

---
+++ migrations/20210731040449-users.js
@@ -0,0 +1,32 @@
+'use strict';
+// Migration
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      isAdmin: Sequelize.BOOLEAN,
+      fullName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+     await queryInterface.dropTable('Users');
+  },
+};
