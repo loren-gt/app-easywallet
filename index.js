ecf62562402288418ec8d822cded5ca887e5733b
Lorena Góes <goesloren@gmail.com>
2021-08-05 08:36:03 -0300

    feat app

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+require('dotenv').config();
+
 const express = require('express');
+
+const route = require('./routes');
  
-const { PORT } = process.env;
+const PORT = process.env.NODE_DOCKER_PORT || 3000;
 
 const app = express();
+
+app.use(express.json());
+app.use(route);
 
 app.get('/', (_req, res) => res.send());
 
