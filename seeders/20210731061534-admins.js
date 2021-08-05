6929b83efb4225d5d3c801b8af3d9a7abc26d14c
Lorena Góes <goesloren@gmail.com>
2021-08-05 08:28:48 -0300

    feat admin seed

--- seeders/20210731061534-admins.js
+++ seeders/20210731061534-admins.js
@@ -1,15 +1,13 @@
 'use strict';
-// Seeder
+
 module.exports = {
   up: async (queryInterface, _Sequelize) => {
     await queryInterface.bulkInsert('Admins',
       [{
-        id: 1,
         userId: 2,
         cnpj: '22.896.431/0001-10',
       },
       {
-        id: 2,
         userId: 4,
         cnpj: '23.759.481/0002-40',
       },
