b6ff7ac98e32ff9b8e2c81d0868eafdbccc1dbd9
Lorena Góes <goesloren@gmail.com>
2021-08-05 08:30:39 -0300

    feat transaction migration

---
+++ migrations/20210803213041-transaction.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Transactions', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      payerId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        defaultValue: 1,
+        references: {
+          model: 'Clients',
+          key: 'userId',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      payeeId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        defaultValue: 1,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      value: Sequelize.FLOAT,
+      published: Sequelize.DATE,
+      updated: Sequelize.DATE,
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('Transactions');
+  }
+};
