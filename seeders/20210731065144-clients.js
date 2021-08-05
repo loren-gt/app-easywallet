1eb517698473746951b919e82a11725432037d63
Lorena Góes <goesloren@gmail.com>
2021-08-05 08:29:16 -0300

    feat client seed

--- seeders/20210731065144-clients.js
+++ seeders/20210731065144-clients.js
@@ -1,15 +1,13 @@
 'use strict';
-// Seeder
+
 module.exports = {
   up: async (queryInterface, _Sequelize) => {
     await queryInterface.bulkInsert('Clients',
       [{
-        id: 1,
         userId: 1,
         cpf: '123.456.789-10',
       },
       {
-        id: 2,
         userId: 3,
         cpf: '321.454.782-59',
       },
